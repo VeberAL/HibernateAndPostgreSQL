3b77285e1b14056e24e4d5531e9c44336f58522d
VeberAL <aleo83@rambler.ru>
2021-11-07 00:30:35 +0300

    Инициализация и настройка Hibernate с БД

---
+++ src/main/java/ru/dbobject/Persons.java
@@ -0,0 +1,16 @@
+package ru.dbobject;
+import lombok.*;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persons{
+    private int P_Id;
+    private String P_FirstName;
+    private String P_LastName;
+    private int P_Age;
+    private String P_Gender;
+    private String P_City;
+}
