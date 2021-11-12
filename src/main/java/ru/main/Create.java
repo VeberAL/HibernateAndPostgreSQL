f898923470f46859f87b5ca947ced5d57855d31b
VeberAL <aleo83@rambler.ru>
2021-11-12 15:12:13 +0300

    Метод Create для создания строки в связанных таблицах БД

--- src/main/java/ru/main/Create.java
+++ src/main/java/ru/main/Create.java
@@ -4,20 +4,28 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.dao.DAO;
 import ru.dao.EmployeesDAO;
+import ru.dao.PersonDAO;
 import ru.dbobject.Employees;
+import ru.dbobject.Person;
 
 public class Create {
     public static void main(String[] args) {
         SessionFactory factory = null;
         try {
             factory = new Configuration().configure().buildSessionFactory();
-            DAO<Employees, String> employeeStringDAO = new EmployeesDAO(factory);
+            DAO<Person, Integer> personDAO = new PersonDAO(factory);
 
-            final Employees employees = new Employees();
-            employees.setE_Id(2);
-            employees.setE_Title("Engineer");
-            employees.setE_Salary(1400);
-            employeeStringDAO.create(employees);
+            final Person person = new Person();
+            person.setP_FirstName("Ben");
+            person.setP_LastName("Kenobi");
+            person.setP_Gender("man");
+            person.setP_Age(58);
+            person.setP_City("Mos Eosli");
+            Employees employees = new Employees();
+            employees.setE_Title("Technique");
+            employees.setE_Salary(1000);
+            person.setEmployees(employees);
+            personDAO.create(person);
         } finally {
             if (factory != null) {
                 factory.close();
