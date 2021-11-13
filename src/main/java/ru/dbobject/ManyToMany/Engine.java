79d131c725d399921e012e8eeeb4c9c0839990e0
VeberAL <aleo83@rambler.ru>
2021-11-14 01:17:02 +0300

    Конфигурация и маппинг для связи ManyToMany

---
+++ src/main/java/ru/dbobject/ManyToMany/Engine.java
@@ -0,0 +1,25 @@
+package ru.dbobject.ManyToMany;
+import lombok.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "cars")
+public class Engine {
+
+    @Getter
+    @Setter
+    private int id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private int power;
+
+    @Getter
+    @Setter
+    private Set<Car> cars;
+
+}
