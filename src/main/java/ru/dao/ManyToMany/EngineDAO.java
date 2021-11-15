7d877d9483b0484675b6402f7eab55a88ec1a189
VeberAL <aleo83@rambler.ru>
2021-11-15 10:53:58 +0300

    Интерфейс для ManyToMany и методы для таблицы Engine

--- src/main/java/ru/dao/ManyToMany/EngineDAO.java
+++ src/main/java/ru/dao/ManyToMany/EngineDAO.java
@@ -1,4 +1,18 @@
 package ru.dao.ManyToMany;
+import com.sun.istack.NotNull;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.dao.DAOMtoM;
+import ru.dbobject.ManyToMany.Engine;
 
-public class EngineDAO {
+public class EngineDAO implements DAOMtoM<Engine, Integer> {
+
+    private final SessionFactory factory;
+    public EngineDAO(@NotNull final SessionFactory factory){this.factory = factory;}
+    @Override
+    public Engine read(@NotNull final Integer id) {
+        try(final Session session = factory.openSession()){
+            return session.get(Engine.class, id);
+        }
+    }
 }
