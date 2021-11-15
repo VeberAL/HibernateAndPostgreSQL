c545ecdd2270d0617b463ae4c82e639f862f05b6
VeberAL <aleo83@rambler.ru>
2021-11-15 12:58:30 +0300

    Read Cars ManyToMany

---
+++ src/main/java/ru/main/ReadCar.java
@@ -0,0 +1,28 @@
+package ru.main;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.dao.DAOMtoM;
+import ru.dao.ManyToMany.CarDAO;
+import ru.dao.ManyToMany.EngineDAO;
+import ru.dbobject.ManyToMany.Car;
+import ru.dbobject.ManyToMany.Engine;
+
+public class ReadCar {
+    public static void main(String[] args) {
+        SessionFactory factory = null;
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            DAOMtoM<Engine, Integer> engineIntegerDAOMtoM = new EngineDAO(factory);
+            DAOMtoM<Car, Integer> carIntegerDAOMtoM = new CarDAO<>(factory);
+            //Вывод на экран двигателя с id=2
+            final Car result = carIntegerDAOMtoM.read(2);
+            System.out.println("Read: " + result);
+        } finally {
+            if (factory != null){
+                factory.close();
+            }
+        }
+    }
+
+}
